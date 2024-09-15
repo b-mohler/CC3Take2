48cfb67861009da7371b7e41be7fcc2e7fe8da9b
b-mohler <98131490+b-mohler@users.noreply.github.com>
2024-09-15 16:37:23 -0400

    Create start_stack.sh

---
+++ start_stack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start the Docker Compose stack in detached mode
+docker-compose up -d
+
+# Wait for user input to stop the stack
+echo "Stack is running. Press [CTRL+C] to stop."
+
+# Wait for the user to stop the services
+trap 'docker-compose down' SIGINT
+wait
